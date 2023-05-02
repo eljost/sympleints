697aa00d46d2689cbaca855f835c63b839b15255
Johannes Steinmetzer <josteinm@posteo.de>
2023-05-02 15:48:13 +0200

    add: function that returns Fortran print function´

--- sympleints/config.py
+++ sympleints/config.py
@@ -1,3 +1,5 @@
 L_MAX = 4  # Up to G by default
 L_AUX_MAX = 6  # Up to I by default
 PREC = 16
+# Base name for arrays in generated code
+ARR_BASE_NAME = "work"
--- sympleints/helpers.py
+++ sympleints/helpers.py
@@ -1,8 +1,10 @@
+from collections.abc import Iterable
 import enum  # from enum import Enum
 import functools
 import itertools as it
 import sys
 import time
+from typing import List, Tuple
 
 from colorama import Fore, Style
 from sympy import IndexedBase, Matrix, Symbol
@@ -33,7 +35,7 @@ class BFKind(enum.Enum):
 RST = Style.RESET_ALL  # colorama reset
 
 
-def canonical_order(L):
+def canonical_order(L: int) -> List[Tuple[int, int, int]]:
     inds = list()
     for i in range(L + 1):
         l = L - i
@@ -43,12 +45,12 @@ def canonical_order(L):
     return inds
 
 
-def sph_order(L):
+def sph_order(L: int) -> List[Tuple[int, int]]:
     # m from -L, -L + 1, ..., 0, 1, ..., L
     return [(L, m) for m in range(-L, L + 1)]
 
 
-def get_order_funcs_for_kinds(kinds):
+def get_order_funcs_for_kinds(kinds: Iterable[BFKind]):
     func_map = {
         BFKind.CART: canonical_order,
         BFKind.SPH: sph_order,
