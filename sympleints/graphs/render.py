d03f53626350c0110cf81be393327924025554e9
Johannes Steinmetzer <josteinm@posteo.de>
2023-05-07 18:09:47 +0200

    add: function to render fortran modules

--- sympleints/graphs/render.py
+++ sympleints/graphs/render.py
@@ -3,19 +3,21 @@ from sympleints.FortranRenderer import format_with_fprettify, get_fortran_print_
 from jinja2 import Environment, PackageLoader, select_autoescape
 
 
+ENV = Environment(
+    loader=PackageLoader("sympleints.graphs"),
+    # We proably don't need autoescape...
+    autoescape=select_autoescape(),
+    trim_blocks=True,
+    lstrip_blocks=True,
+)
+
+
 def render_fortran(gen_integral):
     gi = gen_integral  # Shortcut
 
     print_func = get_fortran_print_func()
-    env = Environment(
-        loader=PackageLoader("sympleints.graphs"),
-        # We proably don't need autoescape...
-        autoescape=select_autoescape(),
-        trim_blocks=True,
-        lstrip_blocks=True,
-    )
     name = gi.integral.name
-    template = env.get_template(f"{name}_func.tpl")
+    template = ENV.get_template(f"{name}_func.tpl")
 
     blocks = dict()
     for block_name, cur_assignments in gi.assignments.items():
@@ -27,6 +29,23 @@ def render_fortran(gen_integral):
         array_defs=gi.array_defs,
         shell_size=gi.shell_size,
         target_array_name=gi.target_array_name,
+    )
+    # Better do this in the module?!
+    # rendered = format_with_fprettify(rendered)
+    return rendered
+
+
+def render_fortran_module(gen_integrals, lmax, lauxmax):
+    gi0 = gen_integrals[0]
+    funcs = [render_fortran(gi) for gi in gen_integrals]
+    L_tots = [gi.L_tots for gi in gen_integrals]
+    tpl = ENV.get_template(f"{gi0.integral.name}_mod.tpl")
+    rendered = tpl.render(
+        integral_name=gi0.integral.name,
+        lmax=lmax,  # TODO
+        lauxmax=lauxmax,  # TODO
+        funcs=funcs,
+        L_tots=L_tots,
     )
     rendered = format_with_fprettify(rendered)
     return rendered
